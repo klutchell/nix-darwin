d94e767685090a4776fd6a02efc5f4d6f279d4e6
Kyle Harding <kyle@balena.io>
2023-10-13 10:43:12 -0400

    Import rich-demo from ryan4yin/nix-darwin-kickstarter @ 8854d9c
    See: https://github.com/ryan4yin/nix-darwin-kickstarter
    Signed-off-by: Kyle Harding <kyle@balena.io>

---
+++ modules/host-users.nix
@@ -0,0 +1,27 @@
+
+{ ... } @ args:
+
+#############################################################
+#
+#  Host & Users configuration
+#
+#############################################################
+
+let
+  # TODO change this to your hostname
+  hostname = "your-hostname";
+  username = "xxx";
+in
+{
+  networking.hostName = hostname;
+  networking.computerName = hostname;
+  system.defaults.smb.NetBIOSName = hostname;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users."${username}" = {
+    home = "/Users/${username}";
+    description = username;
+  };
+
+  nix.settings.trusted-users = [ username ];
+}
