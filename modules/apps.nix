394ffc1f2d7a667972097b675e0e07107205e8c6
Kyle Harding <kyle@balena.io>
2023-10-13 11:55:29 -0400

    Update modules/apps with homebrew targets
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- modules/apps.nix
+++ modules/apps.nix
@@ -21,19 +21,9 @@
     neovim
     git
   ];
-  environment.variables.EDITOR = "nvim";
+  environment.variables.EDITOR = "nano";
 
-  # Homebrew Mirror in China
-  # Feel free to remove this if you are not in China.
-  environment.variables = {
-    HOMEBREW_API_DOMAIN = "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api";
-    HOMEBREW_BOTTLE_DOMAIN = "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles";
-    HOMEBREW_BREW_GIT_REMOTE = "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git";
-    HOMEBREW_CORE_GIT_REMOTE = "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git";
-    HOMEBREW_PIP_INDEX_URL = "https://pypi.tuna.tsinghua.edu.cn/simple";
-  };
-
-  # TODO To make this work, homebrew need to be installed manually, see https://brew.sh
+  # To make this work, homebrew need to be installed manually, see https://brew.sh
   # 
   # The apps installed by homebrew are not managed by nix, and not reproducible!
   # But on macOS, homebrew has a much larger selection of apps than nixpkgs, especially for GUI apps!
@@ -53,7 +43,7 @@
     masApps = {
       # TODO Feel free to add your favorite apps here.
 
-      Xcode = 497799835;
+      # Xcode = 497799835;
       # Wechat = 836500024;
       # NeteaseCloudMusic = 944848654;
       # QQ = 451108668;
@@ -70,37 +60,38 @@
     ];
 
     # `brew install`
-    # TODO Feel free to add your favorite apps here.
     brews = [
       "wget"  # download tool
       "curl"  # no not install curl via nixpkgs, it's not working well on macOS!
       "aria2"  # download tool
       "httpie"  # http client
+      "diffutils"  # diff tool
     ];
 
     # `brew install --cask`
-    # TODO Feel free to add your favorite apps here.
     casks = [
-      "firefox"
-      "google-chrome"
-      "visual-studio-code"
-
-      # IM & audio & remote desktop & meeting
-      "telegram"
+      "balenaetcher"
+      "bitwarden"
+      "brave-browser"
+      "cryptomator"
       "discord"
-
-      "anki"
-      "clashx"    # proxy tool
-      "iina"      # video player
-      "openinterminal-lite"  # open current folder in terminal
-      "syncthing"  # file sync
-      "raycast"   # (HotKey: alt/option + space)search, caculate and run scripts(with many plugins)
-      "iglance"   # beautiful system monitor
-      "eudic"     # 欧路词典
-
-      # Development
-      "insomnia"  # REST client
-      "wireshark"  # network analyzer
+      "docker"
+      "eloston-chromium"
+      "google-drive"
+      "hyper"
+      "logseq"
+      "macfuse"
+      "openinterminal-lite"
+      "openvpn-connect"
+      "protonvpn"
+      "raspberry-pi-imager"
+      "signal"
+      "spotify"
+      "tailscale"
+      "visual-studio-code"
+      "vlc"
+      "zoom"
+      "zulip"
     ];
   };
 }
