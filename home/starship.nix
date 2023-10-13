fe2d06a9a51b808632f6b0cd3c09811823a431df
Kyle Harding <kyle@balena.io>
2023-10-13 11:06:12 -0400

    Revert starship to defaults
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- home/starship.nix
+++ home/starship.nix
@@ -6,20 +6,20 @@
     enableZshIntegration = true;
     enableNushellIntegration = true;
 
-    settings = {
-      character = {
-        success_symbol = "[›](bold green)";
-        error_symbol = "[›](bold red)";
-      };
-      aws = {
-        symbol = "🅰 ";
-      };
-      gcloud = {
-        # do not show the account/project's info
-        # to avoid the leak of sensitive information when sharing the terminal
-        format = "on [$symbol$active(\($region\))]($style) ";
-        symbol = "🅶 ️";
-      };
-    };
+    # settings = {
+    #   character = {
+    #     success_symbol = "[›](bold green)";
+    #     error_symbol = "[›](bold red)";
+    #   };
+    #   aws = {
+    #     symbol = "🅰 ";
+    #   };
+    #   gcloud = {
+    #     # do not show the account/project's info
+    #     # to avoid the leak of sensitive information when sharing the terminal
+    #     format = "on [$symbol$active(\($region\))]($style) ";
+    #     symbol = "🅶 ️";
+    #   };
+    # };
   };
 }
