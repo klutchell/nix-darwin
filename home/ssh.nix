896c6449b2f148947b0aa7ec1af4ca28bbac6a71
Kyle Harding <kyle@balena.io>
2026-06-09 15:09:34 -0400

    fix(home): migrate ssh matchBlocks to settings
    programs.ssh.matchBlocks is deprecated in home-manager 26.05 in favor
    of programs.ssh.settings, which uses upstream OpenSSH directive names.
    Generated ~/.ssh/config is unchanged.
    
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- home/ssh.nix
+++ home/ssh.nix
@@ -5,18 +5,18 @@
     includes = [
       "~/.orbstack/ssh/config"
     ];
-    matchBlocks = {
+    settings = {
       "github.com" = {
-        hostname = "github.com";
-        identityFile = "~/.ssh/id_ed25519";
+        HostName = "github.com";
+        IdentityFile = "~/.ssh/id_ed25519";
       };
       "misc1.dev.balena.io" = {
-        hostname = "misc1.dev.balena.io";
-        user = "klutchell";
+        HostName = "misc1.dev.balena.io";
+        User = "klutchell";
       };
       "misc2.dev.balena.io" = {
-        hostname = "misc2.dev.balena.io";
-        user = "klutchell";
+        HostName = "misc2.dev.balena.io";
+        User = "klutchell";
       };
     };
   };
