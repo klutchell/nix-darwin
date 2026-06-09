948f48c78c50df9604ee7c70ccd3994211dd1701
Kyle Harding <kyle@balena.io>
2026-06-09 15:18:57 -0400

    feat(home): adopt home-manager 26.05 defaults
    Bump home.stateVersion from 23.05 to 26.05, adopting the new gated
    defaults: neovim.withRuby false (drops the unused Ruby provider) and
    yazi.shellWrapperName "y" (was "yy").
    
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- home/default.nix
+++ home/default.nix
@@ -29,7 +29,7 @@ in {
     # You can update Home Manager without changing this value. See
     # the Home Manager release notes for a list of state version
     # changes in each release.
-    stateVersion = "23.05";
+    stateVersion = "26.05";
   };
 
   # Let Home Manager install and manage itself.
